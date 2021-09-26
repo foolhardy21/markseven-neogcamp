af2150577694f88416a26374b4e219490e052513
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-09-26 15:02:59 +0530

    feat - translate text

--- src/index.js
+++ src/index.js
@@ -6,4 +6,34 @@ const mainDiv = document.querySelector('#maindiv')
 mainDiv.appendChild(headerDiv)
 mainDiv.appendChild(contentDiv)
 
-// document.querySelector('.btn').addEventListener('click', translateText)
+function isInputEmpty(input) {
+    return (input.length < 1) ? false : true
+}
+function getUrl(input) {
+    return `https://api.funtranslations.com/translate/navi.json?text=${input}`
+}
+
+function showTranslation(output) {
+    document.querySelector('.outputarea').innerText = output
+}
+
+function translateText(e) {
+    e.preventDefault()
+    const input = document.querySelector('.inputarea').value
+    if( !isInputEmpty(input) ) {
+        window.alert('Enter something')
+    } else {
+        fetch(getUrl(input))
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                showTranslation(data.contents.translated)
+            })
+            .catch(e => {
+                console.log(e)
+            })
+    }
+}
+
+document.querySelector('.btn').addEventListener('click', translateText)
